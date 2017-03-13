ec1a35e729cd8914938912abddc94ad7cc9c6597
ryanthnguyen <qtquoc@memphis.edu>
2017-03-13 03:12:08 -0500

    made changes to the searchBar

---
+++ SearchBar.java
@@ -0,0 +1,27 @@
+//Ryan Nguyen
+public class SearchBar
+{
+    private boolean current;
+    private boolean isManicure;
+    private boolean isPedicure;
+    private boolean isFill;
+    private boolean isFullSet;
+    private int manicure = 15;
+    private int pedicure = 30;
+    private int fill = 15;
+    public SearchBar()
+    {
+        current = false;
+        isManicure = false;
+        isPedicure = false;
+        isFill = false;
+        isFullSet = false;
+    }
+    /* we need a method that will let user enter in what they want to do and then it 
+     * will search through the database and pull that price
+     */
+    public void searchThroughTheArray(String s)
+    {
+        
+    }
+}
